3dfa59b15a4a44a7182fcb9bb54ac5fafba58163
Asher Gilani <asher@AsherStealth.localdomain>
2019-10-31 15:44:48 -0400

    Base search functionality. Basically copied from internet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import logo from './logo.svg';
+import CategoryFilterComponent from './components/filter-component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const countries = 
+    [
+      "Afghanistan",
+      "Aland Islands",
+      "Albania",
+      "Algeria"
+    ]
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <CategoryFilterComponent content={countries} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
