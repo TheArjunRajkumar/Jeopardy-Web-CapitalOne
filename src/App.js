eaf7ca1845d3b1d9f39ce5f2a25e642ab4efb8d8
Asher Gilani <gilaniasher@gmail.com>
2019-11-01 23:08:51 -0400

    Centered Jeopardy image logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import logo from './jeopardy.png'
 import CategoryFilterComponent from './components/filter-component';
 import { Container, Header } from 'semantic-ui-react';
 
+// Settings for stylizing category dropdown
 const styleLink = document.createElement("link");
 styleLink.rel = "stylesheet";
 styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
@@ -12,6 +14,7 @@ class App extends Component {
   render() {
     return (
       <Container style={{ margin: 20 }}>
+        <img src={logo} alt='Jeopardy' className='App-logo' />
         <Header as="h3">Start typing in a category or select one</Header>
         <CategoryFilterComponent/>
       </Container>
